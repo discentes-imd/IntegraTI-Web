3cc39489c852dd9775725f391dbd97150fca7f4c
Weslen Breno B. Soares <weslenbreno@gmail.com>
2017-05-04 04:36:20 -0300

    Adicionando campo Photo para o Model Usuário

--- src/app/_models/usuario.ts
+++ src/app/_models/usuario.ts
@@ -1,7 +1,14 @@
 export class Usuario {
+    public photo:string = '';
+    public habilidades:string[] = []; 
+    
     constructor (
         public nome:string = '',
         public email:string = '',
-        public habilidades:string[] = [] 
-    ){}
+        photo?:string,
+        habilidade?:string[]
+    ){
+        this.photo = photo;
+        this.habilidades = habilidade;
+    }
 }

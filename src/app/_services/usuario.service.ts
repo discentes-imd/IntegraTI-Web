5b43d3ff61464fa17019b36c4ff38d3f9745b269
Weslen Breno B. Soares <weslenbreno@gmail.com>
2017-05-04 04:37:17 -0300

    Adiconando novos usuários na mão

--- src/app/_services/usuario.service.ts
+++ src/app/_services/usuario.service.ts
@@ -5,17 +5,19 @@ import { Usuario } from '../_models/usuario';
 @Injectable()
 export class UsuarioService {
     private users: Usuario[] = [
-        new Usuario('Weslen Breno', 'weslenbreno@gmail.com', ['Angular', 'Django','Javascript']),
-        new Usuario('Sarah Raquel', 'sarah@gmail.com', ['Angular', 'Frontend','Javascript']),    
-        new Usuario('Marcell Guilherme Costa', 'marcell@gmail.com', ['Backend', 'Python','Git']),      
-        new Usuario('Rodrigo Nunes de Castro', 'rodrigondec@gmail.com', ['Backend', 'Python','Git']),      
+        new Usuario('Weslen Breno', 'weslenbreno@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Angular', 'Django','Javascript']),
+        new Usuario('Sarah Raquel', 'sarah@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Angular', 'Frontend','Javascript']),    
+        new Usuario('Marcell Guilherme Costa', 'marcell@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Backend', 'Python','Git']),      
+        new Usuario('Pedro Raoni', 'pedroraoni@integrati.com', 'http://lorempixel.com/240/240/transport/', ['projectManager']),      
+        new Usuario('Gelly Viana', 'gelly@gmail.com', 'http://lorempixel.com/240/240/transport/', ['frontEnd', 'projectManager']),      
+        new Usuario('Rodrigo Nunes de Castro', 'rodrigoc@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Backend', 'Python','Git']),      
     ]
 
     getAllUsers():Array<Usuario>{
         return this.users;
     }
 
-    addNewUser(usuario:Usuario):void {
+    addNewUser(usuario:Usuario){
         this.users.push(usuario);    
     }
 }

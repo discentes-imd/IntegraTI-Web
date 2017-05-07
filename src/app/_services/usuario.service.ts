e46542f543116474ed1cdf5792a58bafbf5ecff1
Weslen Breno B. Soares <weslenbreno@gmail.com>
2017-05-07 05:08:54 -0300

    Adicionando IDs  nos construtores.

--- src/app/_services/usuario.service.ts
+++ src/app/_services/usuario.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 
-import { Usuario } from '../_models/usuario';
+import { Usuario } from '../_models/usuario.model';
 
 @Injectable()
 export class UsuarioService {
+
     private users: Usuario[] = [
-        new Usuario('Weslen Breno', 'weslenbreno@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Angular', 'Django','Javascript']),
-        new Usuario('Sarah Raquel', 'sarah@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Angular', 'Frontend','Javascript']),    
-        new Usuario('Marcell Guilherme Costa', 'marcell@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Backend', 'Python','Git']),      
-        new Usuario('Pedro Raoni', 'pedroraoni@integrati.com', 'http://lorempixel.com/240/240/transport/', ['projectManager']),      
-        new Usuario('Gelly Viana', 'gelly@gmail.com', 'http://lorempixel.com/240/240/transport/', ['frontEnd', 'projectManager']),      
-        new Usuario('Rodrigo Nunes de Castro', 'rodrigoc@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Backend', 'Python','Git']),      
+        new Usuario(0,'Weslen Breno', 'weslenbreno@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Angular', 'Django','Javascript']),
+        new Usuario(1,'Sarah Raquel', 'sarah@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Angular', 'Frontend','Javascript']),    
+        new Usuario(2,'Marcell Guilherme Costa', 'marcell@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Backend', 'Python','Git']),      
+        new Usuario(3,'Pedro Raoni', 'pedroraoni@integrati.com', 'http://lorempixel.com/240/240/transport/', ['projectManager']),      
+        new Usuario(4,'Gelly Viana', 'gelly@gmail.com', 'http://lorempixel.com/240/240/transport/', ['frontEnd', 'projectManager']),      
+        new Usuario(5,'Rodrigo Nunes de Castro', 'rodrigoc@gmail.com', 'http://lorempixel.com/240/240/transport/', ['Backend', 'Python','Git']),      
     ]
 
-    getAllUsers():Array<Usuario>{
+    getAllUsers():Usuario[]{
         return this.users;
     }
 

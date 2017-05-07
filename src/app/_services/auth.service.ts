32323de77eca98b94ccc7d5291fc7416d44af062
Weslen Breno B. Soares <weslenbreno@gmail.com>
2017-05-07 05:07:16 -0300

    Criando lógica de autenticação simples.

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,6 +2,19 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthService {
+    private autheticated: boolean = false;
 
     constructor() { }
+
+    autenticarUsuario(username, password): boolean {
+        if (username === "admin" && password === "admin") {
+            this.autheticated = true;
+            return true;
+        }
+        return false;
+    }
+
+    get isAuthenticated(){
+        return this.autheticated;
+    }
 }

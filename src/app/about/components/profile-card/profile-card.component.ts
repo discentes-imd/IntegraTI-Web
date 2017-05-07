f406396e1ba895595bf662a837fff49a304b242a
Weslen Breno B. Soares <weslenbreno@gmail.com>
2017-05-07 05:13:55 -0300

    Corrigindo erro após mudança no nome da variável.

--- src/app/about/components/profile-card/profile-card.component.ts
+++ src/app/about/components/profile-card/profile-card.component.ts
@@ -13,6 +13,8 @@ export class ProfileCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.habilidades = this.user.habilidades.slice(0,3).map((x) => '#' + x).join(" ")
+    // Mostrando as 3 primeiras tecnologias com # na frente
+    // Da pra fazer mais otimizado com Pipes
+    this.habilidades = this.user.skills.slice(0,3).map((x) => '#' + x).join(" ")
   }
 }

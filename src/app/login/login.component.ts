7bd06ca322031f7f62c5f778bc7195e6f17a7019
Weslen Breno B. Soares <weslenbreno@gmail.com>
2017-05-07 05:17:44 -0300

    Adcionando lógica de auntenticação simples na página de Login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  last_url: string = '/';
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router, private route: ActivatedRoute) { }
+
+  fazerLogin(form: any) {
+    if (this.auth.autenticarUsuario(form.username, form.password)) {
+      this.router.navigate([this.last_url]);
+    }
+  }
 
   ngOnInit() {
+    // Busca a ultima rota para redirecionar após o Login
+    this.route.fragment.subscribe((url) => {
+      this.last_url = url ? url : '/';
+    })
   }
 
 }
